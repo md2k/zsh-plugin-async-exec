00994852b091041206ee4de82a455a8898b1a5d3
Yevgen Flerko <md2k@md2k.net>
2020-12-30 02:08:07 +0100

    testing plugins

---
+++ zplg_async.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+# Async command execution with zinit ice lucid wait
+# https://github.com/zdharma/zinit/issues/68#issuecomment-570776375
+
+zplg_async() {
+  name=$1;
+  cmd=$2;
+  delay=${3:-0}
+  mkdir -p "${ZINIT[PLUGINS_DIR]}/_local---$name"
+  zinit ice lucid wait="$delay" atload="$cmd"
+  zinit light "_local/$name"
+}
