02417184fcd859145306ce3abfdbcadea10b2903
Yevgen Flerko <md2k@md2k.net>
2022-06-10 23:47:03 +0200

    Update async_exec.zsh

--- async_exec.zsh
+++ async_exec.zsh
@@ -6,7 +6,7 @@ async_exec() {
   name=$1;
   cmd=$2;
   delay=${3:-0}
-  mkdir -p "${ZINIT[PLUGINS_DIR]}/_local---$name"
-  zinit ice lucid wait="$delay" atload="$cmd"
-  zinit light "_local/$name"
+  mkdir -p "${ZI[PLUGINS_DIR]}/_local---$name"
+  zi ice lucid wait="$delay" atload="$cmd"
+  zi light "_local/$name"
 }
